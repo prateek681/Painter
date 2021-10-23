995c52aeb5c102d05924637798a12e2ef6759ad4
Lars Wigger <36078827+LarsWigger@users.noreply.github.com>
2021-10-23 14:22:27 +0200

    Added loss functions

--- generator.py
+++ generator.py
@@ -1,4 +1,5 @@
 from torch import nn
+import torch
 
 flatten_dim = 256*256*3
 
@@ -18,6 +19,27 @@ def get_generator_block(input_dim, output_dim):
         nn.BatchNorm1d(output_dim),
         nn.ReLU(inplace=True)
     )
+
+def get_gen_loss(gen, disc, criterion, num_images, photos, device):
+    '''
+    Return the loss of the generator given inputs.
+    Parameters:
+        gen: the generator model, which returns an image given z-dimensional noise
+        disc: the discriminator model, which returns a single-dimensional prediction of real/fake
+        criterion: the loss function, which should be used to compare 
+               the discriminator's predictions to the ground truth reality of the images 
+               (e.g. fake = 0, real = 1)
+        num_images: the number of images the generator should produce, 
+                which is also the length of the real images
+        z_dim: the dimension of the noise vector, a scalar
+        device: the device type
+    Returns:
+        gen_loss: a torch scalar loss value for the current batch
+    '''
+    fake = gen(photos)
+    disc_fake_pred = disc(fake)
+    gen_loss = criterion(disc_fake_pred, torch.ones_like(disc_fake_pred))
+    return gen_loss
 
 class Generator(nn.Module):
     '''
